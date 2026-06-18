4967716d360640a62323ae1c44c07c19aeba9799
202507006 <himang8733@naver.com>
2026-06-18 10:12:25 +0900

    dev/b - mod.cpp, mul.cpp modify

--- calc/mul.cpp
+++ calc/mul.cpp
@@ -1,6 +1,13 @@
-#include "mul.h"
+#include "add.h"
 
 int mul(int x, int y)
 {
-	return 0;
+	/// dev/b - mul.cpp modify
+	int sum=0;
+
+	for (int i = 0;i < y;i++)
+	{
+		sum = add(x, y);
+	}
+	return sum;
 }
