a05d382b59f258e846392318129598c747e51656
202507021 <rkdckdfhr1@gmail.com>
2026-06-18 09:44:59 +0900

    main branch > calc.cpp modify

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -19,11 +19,17 @@ int main()
 	{
 		std::cout << "첫번째 수를 입력하세요 : ";
 		std::cin >> x;
+
+		if (-999 == x)
+		{
+			printf("프로그램을 종료합니다.\n");
+			break;
+		}
 		std::cout << "두번째 수를 입력하세요 : ";
 		std::cin >> y;
 
 		printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
-		printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
+		printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n\n",
 			add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 	}
 }
